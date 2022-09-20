3ed7eb789649871f2fec3eaade4d2d4faecbb0e7
Rossi Kamal <rossikamal@gmail.com>
2022-09-21 04:06:49 +0600

    Update deleteshipment.js

--- src/pages/deleteshipment.js
+++ src/pages/deleteshipment.js
@@ -1,36 +1,59 @@
-import React from 'react'
-import "./App.css";
-import { useHistory } from "react-router-dom";
+import MaterialTable from "material-table";
+import { useEffect, useState } from "react";
 
 function DeleteShipment() {
-	const history = useHistory();
-	const shipmenthome = () => {
-		history.push("/shipment");
-	}
-	return (
-		<>
-			<div className="card " >
-				<h1>Delete Shipment </h1>
-				<ul className="list-group list-group-flush">
-					<li className="list-group-item">
-						Source
-					</li>
-					<li className="list-group-item">
-						Destination
-					</li>
-					<li className="list-group-item">
-						Arrival
-					</li>
-					<li className="list-group-item">
-						Departure
-					</li>
-				</ul>
-			</div>
-			<button className="btn btn-success" onClick={shipmenthome}>
-				Back to Home
-			</button>
-		</>
-	)
+  const columns = [
+    { title: "ID", field: "name", validate: rowData => rowData.name === undefined || rowData.name === "" ? "Empty" : true },
+    { title: "Source", field: "username", validate: rowData => rowData.username === undefined || rowData.username === "" ? "Empty" : true },
+    { title: "Destination", field: "email", validate: rowData => rowData.email === undefined || rowData.email === "" ? "Empty" : true },
+    { title: "Arrival Time", field: "phone", validate: rowData => rowData.phone === undefined || rowData.phone === "" ? "Empty" : true },
+    { title: "Departure Time", field: "website", validate: rowData => rowData.website === undefined || rowData.website === "" ? "Empty" : true }
+  ];
+  const [data, setData] = useState([]);
+  const url = "https://jsonplaceholder.typicode.com/users";
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => {
+        console.error(err.message);
+      })
+  }, []);
+  return (
+    <div>
+      <h1 className="heading">Shipment Information</h1>
+      <p className="heading">Delete Shipment</p>
+      <MaterialTable
+        title="Users Table"
+        columns={columns}
+        data={data}
+        options={{ actionsColumnIndex: -1, addRowPosition: "first" }}
+        editable={{
+        
+       
+          onRowDelete: oldData => new Promise((resolve, reject) => {
+            fetch(url + "/" + oldData.id, {
+              method: "DELETE",
+              headers: {
+                'Content-type': "application/json"
+              },
+
+            })
+              .then(resp => resp.json())
+              .then(resp => {
+                const deletedatda = data.filter(item => item.id !== oldData.id)
+                setData(deletedatda);
+                resolve()
+              })
+          })
+        }}
+      />
+    </div>
+  );
 }
 
-export default DeleteShipment
+export default DeleteShipment;
