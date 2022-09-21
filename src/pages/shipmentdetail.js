5a2858d85fea80be0a93072539f3294b3714791f
Rossi Kamal <rossikamal@gmail.com>
2022-09-21 21:44:47 +0600

    Update shipmentdetail.js

--- src/pages/shipmentdetail.js
+++ src/pages/shipmentdetail.js
@@ -4,7 +4,7 @@ import AppBar from '@material-ui/core/AppBar';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-
+import "./App.css";
 
 export class ShipmentDetail extends Component {
   continue = e => {
