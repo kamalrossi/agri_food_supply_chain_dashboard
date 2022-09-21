98c777ed0ef255ea587cdd547e56afd4d3e9dd5e
Rossi Kamal <rossikamal@gmail.com>
2022-09-21 21:23:30 +0600

    Update ShipmentConfirm.js

--- src/pages/ShipmentConfirm.js
+++ src/pages/ShipmentConfirm.js
@@ -11,7 +11,7 @@ export class ShipmentConfirm extends Component {
     e.preventDefault();
     // PROCESS FORM //
 
- const categoryObject = {
+ const shipmentObject = {
       text: "rossi",
       day: "rossi",
       
