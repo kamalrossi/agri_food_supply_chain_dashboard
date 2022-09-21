dce26d92f28025429d892125033e586a07d09098
Rossi Kamal <rossikamal@gmail.com>
2022-09-21 21:45:59 +0600

    Update shipmentsuccess.js

--- src/pages/shipmentsuccess.js
+++ src/pages/shipmentsuccess.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import AppBar from '@material-ui/core/AppBar';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import "./App.css";
 export class ShipmentSuccess extends Component {
   continue = e => {
     e.preventDefault();
