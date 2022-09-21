3d519c20c01aa7b36910dac1dfeb07d9eab56897
Rossi Kamal <rossikamal@gmail.com>
2022-09-21 21:18:40 +0600

    Add files via upload

---
+++ src/pages/shipmentdetail.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
+export class ShipmentDetail extends Component {
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
+
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
+  render() {
+    const { values, handleChange } = this.props;
+    return (
+      <MuiThemeProvider>
+        <>
+          <Dialog
+            open
+            fullWidth
+            maxWidth='sm'
+          >
+            
+
+            <AppBar title="Enter Shipment Details" />
+      
+              <TextField
+              placeholder="Enter Arrival"
+              label="Arrival"
+              onChange={handleChange('arrival')}
+              defaultValue={values.arrival}
+              margin="normal"
+              fullWidth
+            />
+            <br />
+
+
+   <TextField
+              placeholder="Enter Departure"
+              label="Departure"
+              onChange={handleChange('departure')}
+              defaultValue={values.departure}
+              margin="normal"
+              fullWidth
+            />
+            <br />
+
+       
+
+            <Button
+              color="secondary"
+              variant="contained"
+              onClick={this.back}
+            >Back</Button>
+
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={this.continue}
+            >Continue</Button>
+          </Dialog>
+        </>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default ShipmentDetail;
